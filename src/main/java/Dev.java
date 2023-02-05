a0d02bed2e029d791c95d74ef669ed97f721dfec
Fabrice Theytaz <fabrice.theytaz@gmail.com>
2023-02-05 18:23:02 +0100

    Rien

--- src/main/java/Dev.java
+++ src/main/java/Dev.java
@@ -1,7 +1,18 @@
 
+import java.util.prefs.Preferences;
+
+import io.github.fabricetheytaz.opendocument.IOpenDocument;
+
 public class Dev
 	{
 	public static void main(String[] args) throws Exception
 		{
+		final Preferences preferences = Preferences.userNodeForPackage(IOpenDocument.class);
+
+		//preferences.put("sdd", "sfdsdfsdf");
+		//preferences.flush();
+
+		String dss = preferences.get("sdd", null);
+		System.out.println(dss);
 		}
 	}
